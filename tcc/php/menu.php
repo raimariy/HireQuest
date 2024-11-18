27bc863bb83ec17bb5b26a0c0746a578fcabe938
Raissa Marinho <132758377+raimariy@users.noreply.github.com>
2024-11-18 14:27:55 -0300

    Create menu.php

---
+++ tcc/php/menu.php
@@ -0,0 +1,46 @@
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/menu.css" />  
+    <title>Página - Início</title>
+</head>
+<body>
+    <header>
+        <div class="logus">
+            <img src="../img/logo.svg" alt="logo">
+            <h1>HireQuest</h1>
+        </div>
+        <nav>
+    <ul>
+      <li><a href="#">Sobre</a></li>
+      <li><a href="#">Contato</a></li>
+      <li class="account-container">
+        <a href="#">Já tem uma conta?</a>
+          </label>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+    <main>
+        <aside>
+            <h2><span>Inscreva-se agora</span></h2>
+            <h2>no HireQuest</h2>
+            <p>Conquiste seu lugar no mercado! Aqui você encontra dicas, ferramentas e orientações práticas para impulsionar sua carreira.</p>
+            <form>
+                <input type="submit" value="Começar">
+            </form>
+        </aside>
+        <article>
+            <img src="../img/mulherosa.png" alt="mulher-rosa">
+        </article>
+    </main>
+    <script src="menu.js"></script>
+    <script src="https://kit.fontawesome.com/998c60ef77.js" crossorigin="anonymous"></script>
+</body>
+</html>
